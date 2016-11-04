038185cca59274e3cee6cdc609026c3f8778820b
Thomas GASC <thomas.gasc@mediapart.fr>
2016-11-04 16:36:05 +0100

    document and test AddToSegmentResponse

---
+++ tests/Response/AddToSegmentResponseTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the Mediapart Selligent Client API
+ *
+ * CC BY-NC-SA <https://github.com/mediapart/selligent>
+ *
+ * For the full license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mediapart\Selligent\Response;
+
+use Mediapart\Selligent\Response;
+
+/**
+ *
+ */
+class AddToSegmentResponseTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testResponse()
+    {
+        $response = new AddToSegmentResponse();
+
+        $this->assertEquals(Response::ERROR_QUERY, $response->getCode());
+    }
+}
