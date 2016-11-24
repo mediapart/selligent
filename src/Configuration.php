6a396d68b666ba2f8cb952b5057fccb43618b6fe
Reginald Costa <reginald.costa@gmail.com>
2016-11-24 17:25:51 +0100

    add configuration definition

---
+++ src/Configuration.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Mediapart\Selligent;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+class SelligentConfiguration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('selligent');
+
+        $rootNode
+          ->children()
+            ->scalarNode('login')
+                ->isRequired()
+                ->cannotBeEmpty()
+            ->end()
+            ->scalarNode('password')
+              ->isRequired()
+              ->cannotBeEmpty()
+            ->end()
+            ->scalarNode('namespace')
+                ->defaultValue('http://tempuri.org/')
+            ->end()
+            ->scalarNode('wsdl')
+              ->isRequired()
+              ->cannotBeEmpty()
+            ->end()
+            ->arrayNode('options')
+                ->children()
+                    ->arrayNode('classmap')
+                        ->isRequired()
+                    ->end()
+                ->end()
+            ->end()
+          ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
