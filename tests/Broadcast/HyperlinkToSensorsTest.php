2b75fb0c69808856dbf86a076fd07b478b0e1da2
Thomas GASC <thomas.gasc@mediapart.fr>
2016-12-26 15:49:18 +0100

    handle HYPERLINKS_TO_SENSORS attribute

---
+++ tests/Broadcast/HyperlinkToSensorsTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This file is part of the Mediapart Selligent Client API
+ *
+ * CC BY-NC-SA <https://github.com/mediapart/selligent>
+ *
+ * For the full license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mediapart\Selligent\Tests\Broadcast;
+
+use \XMLWriter;
+use Mediapart\Selligent\Request\CreateCampaign;
+use Mediapart\Selligent\Broadcast\Campaign;
+use Mediapart\Selligent\Broadcast\Target;
+use Mediapart\Selligent\Broadcast\Email;
+
+/**
+ *
+ */
+class HyperlinkToSensorsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testEmailAttribute()
+    {
+        $email = new Email();
+
+        $email->setHyperlinksToSensors(true);
+
+        $this->assertTrue($email->hasHyperlinksToSensors());
+    }
+
+    /**
+     *
+     */
+    public function testCreateCampaign()
+    {
+        $campaign = new Campaign();
+        $target = new Target();
+        $email = new Email();
+        $email
+            ->setTarget($target)
+            ->setHyperlinksToSensors(true)
+        ;
+        $campaign->addEmail($email);
+
+        $writer = new XMLWriter();
+        $request = new CreateCampaign($writer);
+        $xml = $request->basedOn($campaign);
+
+        $document = simplexml_load_string($xml);
+        $this->assertEquals(1, (int) $document->EMAILS->EMAIL[0]->CONTENT->attributes()->HYPERLINKS_TO_SENSORS);
+    }
+}
