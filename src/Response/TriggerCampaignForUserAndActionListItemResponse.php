d269ccb5ff49e8231cbf790a11c58116b1cb962a
Reginald Costa <reginald.costa@gmail.com>
2017-03-31 15:08:49 +0200

    Added class TriggerCampaignForUserAndActionListItem

---
+++ src/Response/TriggerCampaignForUserAndActionListItemResponse.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * This file is part of the Mediapart Selligent Client API
+ *
+ * CC BY-NC-SA <https://github.com/mediapart/selligent>
+ *
+ * For the full license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mediapart\Selligent\Response;
+
+use Mediapart\Selligent\Response;
+
+/**
+ * Trigger the execution of the specified journey map for a specific contact.
+ */
+class TriggerCampaignForUserAndActionListItemResponse extends Response
+{
+    /**
+     * @var int
+     */
+    protected $TriggerCampaignForUserAndActionListItem;
+
+    /**
+     * @var string
+     */
+    protected $result;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->TriggerCampaignForUserAndActionListItem = Response::ERROR_NORESULT;
+        $this->result = '';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCode()
+    {
+        return $this->TriggerCampaignForUserAndActionListItem;
+    }
+
+    /**
+     * Returns a html page if defined in the journey map, else the result is empty
+     *
+     * @return string
+     */
+    public function getResult()
+    {
+        return $this->result;
+    }
+}
